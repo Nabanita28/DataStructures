9664c3d6d4e79ff620e293d05beedf2199177bec
Nabanita Majumder <nabanitamajumder@Nabanitas-MacBook-Air.local>
2020-08-01 23:36:30 +0530

    pratice problems- rotate matrix 90 degrees

---
+++ src/arrays/MatrixRotation.java
@@ -0,0 +1,52 @@
+package arrays;
+
+public class MatrixRotation {
+    public static void main(String[] args) {
+        int[][] arr = new int[][]{{1, 2, 3, 4},
+                                  {5, 6, 7, 8},
+                                  {9, 10, 11, 12},
+                                  {13, 14, 15, 16}};
+        rotateMatrix(arr);
+    }
+
+    private static void rotateMatrix(int[][] array) {
+        int n = array.length;
+        //1st rotation
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < n; c++) {
+                if (r > c) {
+                    int temp = array[r][c];
+                    array[r][c] = array[c][r];
+                    array[c][r] = temp;
+                }
+            }
+        }
+
+        //printMatrix(array);
+        System.out.println("First rotation");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        //2nd rotation
+        for (int i = 0; i < n; i++) {
+            for (int c = 0; c < n/2; c++) {
+                int temp = array[i][c];
+                array[i][c] = array[i][n - 1 - c];
+                array[i][n - 1 - c] = temp;
+            }
+        }
+        //printMatrix(array);
+        System.out.println();
+        System.out.println("2nd rotation");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
