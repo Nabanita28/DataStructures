4717e9bee69f26de5e7d718dab029d111b4eb9b9
Nabanita28 <nab07.09@gmail.com>
2020-08-23 20:27:09 +0530

    Problems on stacks

---
+++ src/stack/ReverseWords.java
@@ -0,0 +1,28 @@
+package stack;
+
+import java.util.Stack;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        String s = "Nabanita Majumder";
+        reverseWords(s);
+
+    }
+
+    private static void reverseWords(String s){
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) != ' '){
+                stack.push(s.charAt(i));
+            } else {
+                while (!stack.isEmpty()) {
+                    System.out.print(stack.pop());
+                }
+                System.out.print(" ");
+            }
+        }
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop());
+        }
+    }
+}
