385b99aeddd2bc30f780bd8e56c6d7e8cbd45736
Nabanita28 <nab07.09@gmail.com>
2020-09-02 01:12:37 +0530

    Tree problems - LCA problems

--- src/tree/BasicTreeFunctions.java
+++ src/tree/BasicTreeFunctions.java
@@ -16,14 +16,14 @@ public class BasicTreeFunctions {
     }
 
     public static TreeNode createTree() {
-        root = new TreeNode(9);
-        root.left = new TreeNode(8);
-        root.right = new TreeNode(6);
-        root.left.left = new TreeNode(5);
-        root.left.right = new TreeNode(2);
-        root.right.left = new TreeNode(1);
-        /*root.right.right = new TreeNode(1);
-        root.left.left.left = new TreeNode(8);
+        root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(5);
+        root.right.left = new TreeNode(6);
+        root.right.right = new TreeNode(7);
+        /*root.left.left.left = new TreeNode(8);
         root.left.left.right = new TreeNode(9);
         root.left.right.left = new TreeNode(10);
         root.left.right.right = new TreeNode(11);
