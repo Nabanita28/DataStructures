3ba789dc266f8c2185983fe4a76b3edefe3965e0
Nabanita28 <nab07.09@gmail.com>
2020-09-09 20:38:53 +0530

    Square roor of a number

---
+++ src/general/Test.java
@@ -0,0 +1,13 @@
+package general;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+        int[] arr = { 13, 7, 6, 45, 21, 9, 101, 102 };
+
+        Arrays.sort(arr);
+
+        System.out.printf(arr.toString());
+    }
+    }
