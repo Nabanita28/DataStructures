7c9a2a7ae0e8680893deab8373f2812b0aac70d1
Nabanita28 <nab07.09@gmail.com>
2020-09-14 02:31:32 +0530

    Left/Right/bottom Tree View

--- src/tree/TreeNode.java
+++ src/tree/TreeNode.java
@@ -2,14 +2,16 @@ package tree;
 
 public class TreeNode {
 
-    TreeNode left;
-    int key;
-    TreeNode right;
-    TreeNode root;
+    public TreeNode left;
+    public int key;
+    public TreeNode right;
+    public TreeNode root;
+    public int hd;
 
     public TreeNode(int key){
         this.key = key;
         this.left = null;
         this.right = null;
+        this.hd = 0;
     }
 }
