5aeb768cf39889d482c6e2c55e3699b63811907c
Nabanita28 <nab07.09@gmail.com>
2020-09-13 21:05:08 +0530

    String Permutation

---
+++ src/Strings/StringPermutation.java
@@ -0,0 +1,35 @@
+package Strings;
+
+public class StringPermutation {
+
+    public static void main(String[] args) {
+        String s = "ABC";
+        //TODO need to re-visit
+        printPermutations(s, 0, s.length()-1);
+
+    }
+
+    private static void printPermutations(String str, int left, int right) {
+
+        if (left == right)
+            System.out.println(str.toString());
+
+        else{
+            for(int i=left; i<=right; i++){
+             str = swap(str, left, i);
+             printPermutations(str, left+1, right);
+             str = swap(str, left, i);
+            }
+        }
+    }
+
+    private static String swap(String str, int left, int right){
+        char[] c = str.toCharArray();
+
+        char temp = c[left];
+        c[left] = c[right];
+        c[right] = temp;
+
+        return String.valueOf(c);
+    }
+}
