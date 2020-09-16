7d3c049d5419a64e5a118517e9e0fd7564361eb3
Nabanita28 <nab07.09@gmail.com>
2020-09-17 01:39:48 +0530

    Check if tree is BST

---
+++ src/tree/mustDo/CheckIfBST.java
@@ -0,0 +1,39 @@
+package tree.mustDo;
+
+import tree.TreeNode;
+
+public class CheckIfBST {
+
+    public static void main(String[] args) {
+
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(11);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(6);
+
+
+          /* root.left = new TreeNode(2);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(4);*/
+
+        System.out.println(checkIfBST(root));
+    }
+
+    private static boolean checkIfBST(TreeNode root) {
+
+        if (root == null)
+            return true;
+
+        if (root.left != null && root.left.key > root.key )
+            return false;
+
+        if (root.right != null && root.right.key < root.key)
+            return false;
+
+        return  checkIfBST(root.left) && checkIfBST(root.right);
+
+    }
+
+}
