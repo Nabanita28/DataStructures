a36fe8217d86353b2d7663a00a3955629ec287b9
Nabanita28 <nab07.09@gmail.com>
2020-10-09 15:44:43 +0530

    Maximum Number of 1 In Sorted Matrix

---
+++ src/arrays/mockInterviews/MaximumNumberof1InSortedMatrix.java
@@ -0,0 +1,23 @@
+package arrays.mockInterviews;
+
+public class MaximumNumberof1InSortedMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]
+                {
+                        {0,1,1,1},
+                        {0,0,1,1},
+                        {1,1,1,1},
+                        {0,0,0,0}
+                };
+        findMaximumNumberOf1InARow(matrix);
+    }
+
+    private static void findMaximumNumberOf1InARow(int[][] matrix) {
+
+    }
+
+
+    private static void findMaximumNumberOf1InARowByGogo(int[][] matrix) {
+
+    }
+}
