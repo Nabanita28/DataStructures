8baa3996c2887de94e83d2a12c8d194330857fd5
Nabanita28 <nab07.09@gmail.com>
2021-09-03 23:05:49 +0530

    Print all paths from root

--- src/dataStructuresPractice/tree/PrintAllPathsFromRoot.java
+++ src/dataStructuresPractice/tree/PrintAllPathsFromRoot.java
@@ -17,6 +17,8 @@ public class PrintAllPathsFromRoot {
 
         List<String> paths = binaryTreePaths(root);
         paths.stream().forEach(path -> System.out.println(path));
+
+        System.out.println(checkIfSumIsPresent(root, 3));
     }
 
     static  public List<String> binaryTreePaths(TreeNode root) {
@@ -32,7 +34,7 @@ public class PrintAllPathsFromRoot {
             return;
         }
 
-        path = path + "->" + String.valueOf(root.key);
+        path = path + "->" + root.key;
 
         if(root.left == null && root.right == null){
             paths.add(path.substring(2));
@@ -41,5 +43,24 @@ public class PrintAllPathsFromRoot {
             generatePaths(root.right, paths, path);
         }
 
+    }
+
+    static private boolean checkIfSumIsPresent(TreeNode root, int sum) {
+
+        if (sum == 0) {
+            return true;
+        } else {
+
+            int remainingSum = sum - root.key;
+
+            if ((root.left != null && root.right != null) || (root.left == null && root.right == null)) {
+                return checkIfSumIsPresent(root.left, remainingSum) || checkIfSumIsPresent(root.right, remainingSum);
+            }
+            else if(root.left != null){
+                return checkIfSumIsPresent(root.left, remainingSum);
+            }else{
+                return checkIfSumIsPresent(root.right, remainingSum);
+            }
+        }
     }
 }
