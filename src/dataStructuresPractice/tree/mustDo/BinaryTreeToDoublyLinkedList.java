46adca27c125aff5b0418891958e5d6c81aa0fd2
Nabanita28 <nab07.09@gmail.com>
2021-09-07 14:32:03 +0530

    BST to Circular DLL

--- src/dataStructuresPractice/tree/mustDo/BinaryTreeToDoublyLinkedList.java
+++ src/dataStructuresPractice/tree/mustDo/BinaryTreeToDoublyLinkedList.java
@@ -3,27 +3,86 @@ package dataStructuresPractice.tree.mustDo;
 import dataStructuresPractice.tree.TreeNode;
 
 public class BinaryTreeToDoublyLinkedList {
+
+    static class Tree {
+        TreeNode root;
+
+        Tree() {
+            root = null;
+        }
+    }
+
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(10);
-        root.left = new TreeNode(12);
-        root.right = new TreeNode(15);
-        root.left.left = new TreeNode(25);
-        root.left.right = new TreeNode(30);
-        root.right.left = new TreeNode(36);
+        Tree tree = new Tree();
+        tree.root = new TreeNode(10);
+        tree.root.left = new TreeNode(12);
+        tree.root.right = new TreeNode(15);
+        tree.root.left.left = new TreeNode(25);
+        tree.root.left.right = new TreeNode(30);
+        tree.root.right.left = new TreeNode(36);
+
         System.out.println("Binary Tree To Doubly Linked List");
-        binaryToDoublyLinkedList(root);
-        print();
+        //binaryToDoublyLinkedList(tree.root);
+        TreeNode node = BSTtoCircularDLL(tree.root);
+        print(node);
+    }
+
+    private static TreeNode BSTtoCircularDLL(TreeNode root) {
+
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = BSTtoCircularDLL(root.left);
+        TreeNode right = BSTtoCircularDLL(root.right);
+
+        root.left = root;
+        root.right = root;
+
+        return concatenate(concatenate(left, root), right);
+
+    }
+
+    static TreeNode concatenate(TreeNode leftList, TreeNode rightList) {
+        if (leftList == null) {
+            return rightList;
+        }
+        if (rightList == null) {
+            return leftList;
+        }
+
+        TreeNode leftLastNode = leftList.left;
+        TreeNode rightLastNode = rightList.left;
+
+        leftLastNode.right = rightList;
+        rightList.left = leftLastNode;
+
+        leftList.left = rightLastNode;
+        rightLastNode.right = leftList;
+        return leftList;
+
     }
 
     private static void print() {
-        while (head != null){
+        while (head != null) {
             System.out.print(head.key + " ");
             head = head.right;
         }
     }
 
+    static public void print(TreeNode head) {
+        System.out.println("Circular Linked List is :");
+        TreeNode current = head;
+        while (current.right != head) {
+            System.out.print(current.key + " ");
+            current = current.right;
+        }
+        System.out.println();
+    }
+
     static TreeNode prev = null;
     static TreeNode head;
+
     private static void binaryToDoublyLinkedList(TreeNode node) {
 
         if (node == null)
@@ -31,11 +90,10 @@ public class BinaryTreeToDoublyLinkedList {
 
         binaryToDoublyLinkedList(node.left);
 
-        if (prev == null){
+        if (prev == null) {
             //mark node as head
             head = node;
-        }
-        else{
+        } else {
             node.left = prev;
             prev.right = node;
         }
